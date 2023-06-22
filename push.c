dc81106c7add98ff73ad8c4d39d13b853f392503
Tinodana <abinarac@gmail>
2023-06-22 16:27:40 -0700

    Trying again

---
+++ push.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+
+/**
+ * f_push - add node to the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+
+void f_push(stack_t **head, unsigned int counter)
+{
+	int j, i = 0, flag = 0;
+
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			i++;
+		for (; bus.arg[i] != '\0'; i++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	j = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, j);
+	else
+		addqueue(head, j);
+}
